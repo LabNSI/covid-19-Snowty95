b9be4f1806607fd91770c8bd8104b68d4edae2e4
Snowty <snowty.3ball@gmail.com>
2020-03-31 15:03:02 +0200

    V2 et V3 finis

--- covid-19v2.py
+++ covid-19v2.py
@@ -4,7 +4,7 @@
 # Author : MS
 # Date : 16/03/2020
 #
-# Replace the "___" by appropriate code
+# Replace the "Snowtyy" by appropriate code
 #
 ###########################################
 
@@ -17,17 +17,17 @@ def download_file(url, file_name):
     '''
     try:
         # open url passed in argument
-        file = urllib2.urlopen(___)
+        file = urllib2.urlopen(url)
         # open file for writing in binary mode
         # https://docs.python.org/3/library/functions.html#open
-        with open(___, '___') as output:
+        with open(file_name, "wb") as output:
             # write in output the file read
-            output.___(file.___())
+            output.write(file.read())
         # OK : return true
-        return ___
+        return True
     except:
         # Something is wrong : return false
-        return ___
+        return False
 
 def read_CSV(file):
     '''
@@ -41,11 +41,11 @@ def read_CSV(file):
     # https://docs.python.org/3/library/csv.html#csv.reader
     with open(file) as csvfile:
         # Create a dictionnary with all datas
-        reader = csv.DictReader(____)
+        reader = csv.DictReader(csvfile)
         # each row in reader is a dictionary by country
         for row in reader:
             # add row in datas list
-            datas.___(___)
+            datas.append(row)
         # return the list of dictionaries by countries
         return datas
 
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     # File name of datas
     file = "time_series_19-covid-Confirmed.csv"
     # where datas are located
-    url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
     if download_file(url,file):
         print(f'Téléchargement du fichier {file} terminé avec succès')
--- covid-19v3.py
+++ covid-19v3.py
@@ -4,7 +4,7 @@
 # Author : MS
 # Date : 16/03/2020
 #
-# Replace the "___" by appropriate code
+# Replace the "Snowtyy" by appropriate code
 #
 ###########################################
 
@@ -18,17 +18,17 @@ def download_file(url, file_name):
     '''
     try:
         # open url passed in argument
-        file = urllib2.urlopen(___)
+        file = urllib2.urlopen(url)
         # open file for writing in binary mode
         # https://docs.python.org/3/library/functions.html#open
-        with open(___, '___') as output:
+        with open(file_name, "wb") as output:
             # write in output the file read
-            output.___(file.___())
+            output.write(file.read())
         # OK : return true
-        return ___
+        return True
     except:
         # Something is wrong : return false
-        return ___
+        return False
 
 
 def read_CSV(file):
@@ -43,40 +43,42 @@ def read_CSV(file):
     # https://docs.python.org/3/library/csv.html#csv.reader
     with open(file) as csvfile:
         # Create a dictionnary with all datas
-        reader = csv.DictReader(____)
+        reader = csv.DictReader(csvfile)
         # each row in reader is a dictionary by country
         for row in reader:
             # add row in datas list
-            datas.___(___)
+            datas.append(row)
         # return the list of dictionaries by countries
         return datas
 
-def data_for_country(data, state = "", country = ""):
+
+def data_for_country(data, state="", country=""):
     # for each country in data
-    for pays in ___:
+    for pays in data:
         # if 'Province\State' key match with state given in argument
         # and if 'Country/Region' key match with country given in argument
-        if pays[___] == ___ and pays[___] == ___:
+        if pays['Country/Region'] == country and pays['Province/State'] == state:
             return pays
 
 
 # here the main code
 if __name__ == '__main__':
 
-  # File name of datas
-  file = "time_series_19-covid-Confirmed.csv"
-  # where datas are located
-  url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    # File name of datas
+    file = "time_series_19-covid-Confirmed.csv"
+    # where datas are located
+    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
+          "/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv "
 
-  if download_file(url,file):
-      print(f'Téléchargement du fichier {file} terminé avec succès')
-      countries = read_CSV(file)
-      print("liste des pays et régions recencés")
-      print("----------------------------------")
-      # for row in coutries:
-      #    print(row['Province/State'], row['Country/Region'])
-      france = data_for_country(countries, 'France', 'France')
-      print(france)
+    if download_file(url, file):
+        print(f'Téléchargement du fichier {file} terminé avec succès')
+        countries = read_CSV(file)
+        print("liste des pays et régions recencés")
+        print("----------------------------------")
+        # for row in coutries:
+        #    print(row['Province/State'], row['Country/Region'])
+        france = data_for_country(countries, '', 'France')
+        print(france)
 
-  else:
-      print(f'Téléchargement du fichier {file} impossible')
+    else:
+        print(f'Téléchargement du fichier {file} impossible')
